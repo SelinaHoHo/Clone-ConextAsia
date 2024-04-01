09b1405d14882209cb72127ef0f1dff36f197fcb
SelinaHoHo <ngocthanhnbk123@gmail.com>
2024-04-01 11:27:00 +0700

    update init FE

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>vfff</h1>
-      <h2>gfghxt</h2>
-    </>
+    <div>
+      <h1 className="text-red-500">selina
+      </h1>
+    </div>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: "767px",
+        md: "899px",
+        lg: "1024px",
+      },
+      fontFamily: {
+        apple: [--apple-system, "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif"],
+      },
+      extend: {
+        spacing: {
+          128: "32rem",
+          144: "36rem",
+        },
+        borderRadius: {
+          "4xl": "2rem",
+        },
+      },
+      colors: {
+        blueMain: "#171C31",
+        yellowMain: "#FED60",
+        blueFooter: "#0E1B28",
+      },
+    },
+  },
+  plugins: [],
+}
