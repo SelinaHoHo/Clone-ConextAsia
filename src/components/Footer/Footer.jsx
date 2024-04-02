06c1e5e2b42db947038412226b2bd01d9ce10fa3
KhanhNguyen2710 <71586925+KhanhNguyen2710@users.noreply.github.com>
2024-04-02 17:20:23 +0700

    Feat/cap 1 (#2)

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,174 @@
+import { Col, Row } from "antd";
+import heppi from "../../assets/Img/heppi.png";
+import st from "../../assets/Img/St.png";
+import ciao from "../../assets/Img/Ciao.png";
+import iot from "../../assets/Img/IoT.png";
+import Madeln from "../../assets/Img/Madeln.png";
+import logoFooter from "../../assets/Img/logo_footer.png";
+import faicon from "../../assets/Img/icon/facebook-icon-white.png";
+import inicon from "../../assets/Img/icon/linkedin-icon-white.png";
+import { useTranslation } from "react-i18next";
+import i18n from "../../../i18n";
+
+const Footer = () => {
+  const { t } = useTranslation();
+
+  return (
+    // <div >Footer</div>
+    
+    <>
+      <div className="bg-[#0E1B28]  py-12">
+        {" "}
+        {/* <Row className="text-white items-center md:mx-20 ">
+          <Col xs={8} sm={8} md={8} lg={4} xl={4} className="mx-auto">
+            <div className="w-50% p-3">
+              <img
+                src={heppi}
+                className="lg:w-[50%] sm:w-[80%] xs:w-[70%] "
+              ></img>
+            </div>
+          </Col>
+          <Col xs={8} sm={8} md={8} lg={4} xl={4} className="mx-auto">
+            <div className="w-50% p-3">
+              <img src={st} className="lg:w-[25%] sm:w-[40%] xs:w-[30%] "></img>
+            </div>
+          </Col>
+          <Col xs={8} sm={8} md={8} lg={4} xl={4} className="mx-auto">
+            <div className="w-50% p-3">
+              <img
+                src={ciao}
+                className="lg:w-[50%] sm:w-[80%] xs:w-[70%] "
+              ></img>
+            </div>
+          </Col>
+          <Col xs={8} sm={8} md={8} lg={4} xl={4}>
+            <div className="w-50% p-3">
+              <img
+                src={iot}
+                className="lg:w-[35%] sm:w-[40%]  xs:w-[30%]"
+              ></img>
+            </div>
+          </Col>
+          <Col xs={8} sm={8} md={8} lg={4} xl={4}>
+            <div className="w-50% p-3">
+              <img src={Madeln} className="lg:w-[100%]"></img>
+            </div>
+          </Col>
+        </Row> */}
+        <Row className="text-white items-center md:mx-20 sm:mx-12 xs:mx-10">
+          <Col xs={8} sm={8} md={8} lg={4} xl={4} className="mx-auto">
+            <div className="w-50% p-3">
+              <a href="https://heppi.net/">
+                <img
+                  src={heppi}
+                  className="lg:w-[100%] sm:w-[80%] xs:w-[70%]"
+                  alt="Heppi"
+                />
+              </a>
+            </div>
+          </Col>
+          <Col xs={8} sm={8} md={8} lg={4} xl={4} className="mx-auto">
+            <a href="https://stunited.vn/" className="flex justify-center p-3">
+              <img
+                src={st}
+                className="lg:w-[55%] sm:w-[40%] xs:w-[30%]"
+                alt="St"
+              />
+            </a>
+          </Col>
+          <Col xs={8} sm={8} md={8} lg={4} xl={4} className="mx-auto ">
+            <a
+              href="https://ciaoflora.com/"
+              className="flex justify-center p-3"
+            >
+              <img
+                src={ciao}
+                className="lg:w-[100%] sm:w-[80%] xs:w-[70%]"
+                alt="Ciao"
+              />
+            </a>{" "}
+          </Col>
+          <Col xs={8} sm={8} md={8} lg={4} xl={4}>
+            <a
+              href="link_đến_trang_cần_chuyển_hướng"
+              className="flex justify-center  p-3"
+            >
+              <img
+                src={iot}
+                className="lg:w-[70%] sm:w-[40%] xs:w-[30%]"
+                alt="Iot"
+              />
+            </a>
+          </Col>
+          <Col xs={8} sm={8} md={8} lg={4} xl={4}>
+            <a
+              href="https://madeindanang.com/"
+              className="flex justify-center p-3"
+            >
+              <img src={Madeln} className="lg:w-[100%]" alt="Madeln" />
+            </a>
+          </Col>
+        </Row>
+        <span className="bg-slate-600 h-px w-11/12 mx-auto lg:my-11 md:mb-5 sm:my-2 xs:mb-7 block"></span>
+        <Row className="text-white items-center md:mx-20 xs:mx-5">
+          <Col xs={10} sm={6} md={5} lg={5} xl={5}>
+            <img
+              src={logoFooter}
+              className="w-[50%] truncate hidden md:block"
+            />
+            <p className="text-[0.75rem] mt-5 text-[#5c6369]">© 2020 CoNEXT</p>
+          </Col>
+          <Col xs={0} sm={0} md={0} lg={16} xl={16}>
+            <Row className=" uppercase  ">
+              <Col span={6}>
+                <p className="text-[0.75rem]  lg:text-[0.75rem] "> {t("Event")}</p>
+                <p className="text-[0.75rem]  lg:text-[0.75rem] mt-7">
+                {t("hot")}
+                </p>
+              </Col>
+              <Col span={6}>
+                <p className="text-[0.75rem] lg:text-[0.75rem] ">{t("workplaces")}</p>
+                <p className="text-[0.75rem]  lg:text-[0.75rem] mt-7">
+                  {t("t.a.p")}
+                </p>
+              </Col>
+              <Col span={6}>
+                <p className="text-[0.75rem]  lg:text-[0.75rem] ">{t("voucher")}</p>
+                <p className="text-[0.75rem] lg:text-[0.75rem]  mt-7">
+                {t("ab")}
+                </p>
+              </Col>
+              <Col span={6}>
+                <p className="text-[0.75rem] lg:text-[0.75rem]">{t("faq")}</p>
+                <p className="text-[0.75rem]  lg:text-[0.75rem]  mt-7">
+                {t("con")}
+                </p>
+              </Col>
+            </Row>
+          </Col>
+          <Col
+            xs={14}
+            sm={18}
+            md={19}
+            lg={3}
+            xl={3}
+            className="flex justify-end gap-3 "
+          >
+            <img
+              src={faicon}
+              alt=""
+              className=" lg:w-[15%] lg:h-[15%]  md:w-[10%] sm:w-[5%]  xs:w-[7%]  "
+            />
+            <img
+              src={inicon}
+              alt=""
+              className=" lg:w-[15%] lg:h-[15%] md:w-[10%]  sm:w-[5%] xs:w-[7%]"
+            />
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,139 @@
+import { Col, Dropdown, List, Menu, Row, Select, Typography } from "antd";
+import LogoNav from "../../assets/Img/LogoNav.png";
+import Ava from "../../assets/Img/default_avatar.png";
+import {
+  BellOutlined,
+  GiftOutlined,
+  MenuOutlined,
+  MessageOutlined,
+} from "@ant-design/icons";
+import LanguageSwitcher from "../LanguageSwitcher";
+import i18n from "../../../i18n";
+import Sidebar from "../Sidebar/Sidebar";
+import { useState } from "react";
+
+const Header = () => {
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+
+  const data = ["Opp!There is no new update ... "];
+  const truncateName = (name, maxLength) => {
+    if (name.length <= maxLength) return name;
+    return name.substring(0, maxLength) + "...";
+  };
+  const menu = (
+    <Menu className="w-[300px]">
+      <List
+        footer={
+          <div>
+            {" "}
+            <Menu.Item key="1" className="text-center">
+              show all{" "}
+            </Menu.Item>
+          </div>
+        }
+        bordered
+        dataSource={data}
+        renderItem={(item) => (
+          <List.Item>
+            <Typography.Text mark></Typography.Text> {item}
+          </List.Item>
+        )}
+      />
+    </Menu>
+  );
+
+  const name = "show ten nguoi dung";
+  const maxLength = 10;
+  return (
+    <>
+      <Row className="bg-[#171C32] px-5 py-4 text-white items-center	">
+        <Col xs={8} sm={5} md={4} lg={4} xl={3}>
+          <a href="#">
+            <div className="w-4/5">
+              <img
+                src={LogoNav}
+                style={{ maxWidth: "100%", height: "auto" }}
+                alt=""
+              />
+            </div>
+          </a>
+        </Col>
+        <Col xs={0} sm={0} md={7} lg={7} xl={8}>
+          <div>
+            <Select
+              className="lg:w-1/2 sm:w-full h-[2.5rem] hidden sm:block  "
+              showSearch
+              defaultValue="1"
+              optionFilterProp="children"
+              filterOption={(input, option) =>
+                (option?.label ?? "").includes(input)
+              }
+              filterSort={(optionA, optionB) =>
+                (optionA?.label ?? "")
+                  .toLowerCase()
+                  .localeCompare((optionB?.label ?? "").toLowerCase())
+              }
+              options={[
+                {
+                  value: "1",
+                  label: "Da Nang",
+                },
+                {
+                  value: "2",
+                  label: "Ha Noi",
+                },
+                {
+                  value: "6",
+                  label: "Hoi Chi Minh",
+                },
+              ]}
+            />
+          </div>
+        </Col>
+        <Col
+          xs={13}
+          sm={17}
+          md={11}
+          lg={12}
+          xl={12}
+          className="flex justify-end lg:gap-10 gap-4"
+        >
+          <div className="flex gap-4 items-center">
+            <LanguageSwitcher />
+
+            <GiftOutlined style={{ fontSize: "1.4rem" }} />
+            <MessageOutlined style={{ fontSize: "1.4rem" }} />
+
+            <Dropdown overlay={menu} placement="bottom" arrow>
+              <BellOutlined style={{ fontSize: "1.4rem" }} />
+            </Dropdown>
+          </div>
+          <div className="flex gap-3 items-center ">
+            <img src={Ava} alt="avatar" className="w-9" />
+            <p className="truncate hidden sm:block">
+              {truncateName(name, maxLength)}
+            </p>
+          </div>
+        </Col>
+
+        <Col xs={1} sm={1} md={1} lg={0} xl={0}>
+          <div>
+            <MenuOutlined className="text-2xl , ml-6" onClick={toggleSidebar} />
+            <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar}/>
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Header;
