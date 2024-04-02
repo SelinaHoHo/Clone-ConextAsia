f9ca2f163a5c570991dc57a16fe307f37562117f
trandangquang <trandangquang2203@gmail.com>
2024-04-02 14:59:44 +0700

    feat/CAP-15: create card booking

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,19 @@
-import React from 'react'
-import { useTranslation } from "react-i18next";
-import i18n from "../../i18n";
-import LanguageSwitcher from "../components/LanguageSwitcher";
-import Header from '../components/Header/Header';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
 
 const HomePage = () => {
   const { t } = useTranslation();
 
   return (
     <div>
-      {/* <LanguageSwitcher />
-      <h1>{t("hello")}</h1>
-      <p>{t("welcome")}</p> */}
-      <Header/>
-      <p>body</p>
-      <h1>{t("hello")}</h1>
-      <p>{t("welcome")}</p> 
-      <Footer/>
+      <Header />
+      <h1>{t('hello')}</h1>
+      <p>{t('welcome')}</p>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
