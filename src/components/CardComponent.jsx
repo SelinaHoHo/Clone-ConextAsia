4b3b0491c4e99acb261c73028c6e57c65817f3ca
trandangquang <trandangquang2203@gmail.com>
2024-04-03 13:25:43 +0700

    feat/CAP-15: fix responsive

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable react/prop-types */
+
 import { Col, Row } from 'antd';
 import './Card.scss';
 
 const CardComponent = ({ data }) => {
   return (
-    <Row className='container mx-auto lg:flex block mb-5 mt-5'>
+    <Row className='mb-5 mt-5 flex'>
       <Col
         span={12}
-        className='block w-full md:flex lg:gap-5 gap-8 rounded-md shadow-lg h-fit max-w-fit px-5 lg:px-0'
+        className='sm:flex min-w-full md:min-w-0 block gap-8 rounded-md shadow-lg px-5 '
       >
-        <div className='lg:w-[70%] w-full overflow-hidden'>
+        <div className='w-full overflow-hidden'>
           <img
             className='block h-full w-full transition-transform duration-500 transform hover:scale-110 rounded-md '
             src={
@@ -19,7 +20,7 @@ const CardComponent = ({ data }) => {
             alt='card-image'
           />
         </div>
-        <div className='py-[20px] w-full pr-[32px]'>
+        <div className='py-[20px] w-full'>
           <div>
             <h3 className='text-[20px] font-semibold '>{data?.name}</h3>
             <p className='mb-4 text-base'>{data.address}</p>
@@ -32,7 +33,6 @@ const CardComponent = ({ data }) => {
             <button className='relative btn-shadow font-medium text-black px-[18px] py-[12px] bg-[#fed702] rounded transition-transform duration-300 hover:translate-y-[-5px] group'>
               BOOK A SEAT
             </button>
-
             <a className='text-[#1890ff] hover:text-[#b6deff]' href='#'>
               See more
             </a>
