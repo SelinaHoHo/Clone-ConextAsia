a16ffdc877d24e6c82fb9224333cdc38dd292149
trandangquang <trandangquang2203@gmail.com>
2024-04-03 16:05:20 +0700

    feat/CAP-17: filter and search

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,17 @@
-import { Col, Dropdown, List, Menu, Row, Select, Typography } from "antd";
-import LogoNav from "../../assets/Img/LogoNav.png";
-import Ava from "../../assets/Img/default_avatar.png";
 import {
   BellOutlined,
   GiftOutlined,
   MenuOutlined,
   MessageOutlined,
-} from "@ant-design/icons";
-import LanguageSwitcher from "../LanguageSwitcher";
-import Sidebar from "../Sidebar/Sidebar";
-import { useState } from "react";
+} from '@ant-design/icons';
+import { Col, Dropdown, List, Menu, Row, Select, Typography } from 'antd';
+import { useState } from 'react';
+import LogoNav from '../../assets/Img/LogoNav.png';
+import Ava from '../../assets/Img/default_avatar.png';
+import LanguageSwitcher from '../LanguageSwitcher';
+import Sidebar from '../Sidebar/Sidebar';
 
-const Header = () => {
-
+const Header = ({ onSearchTermChange }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const toggleSidebar = () => {
@@ -22,20 +21,30 @@ const Header = () => {
     setIsSidebarOpen(false);
   };
 
-
-  const data = ["Opp!There is no new update ... "];
+  const data = ['Opp!There is no new update ... '];
   const truncateName = (name, maxLength) => {
     if (name.length <= maxLength) return name;
-    return name.substring(0, maxLength) + "...";
+    return name.substring(0, maxLength) + '...';
   };
+
+  const [selectedLabel, setSelectedLabel] = useState('Da Nang');
+  console.log(selectedLabel);
+
+  const handleSelectChange = (value, option) => {
+    if (option) {
+      setSelectedLabel(option.label);
+      onSearchTermChange(option.label);
+    }
+  };
+
   const menu = (
-    <Menu className="w-[300px]">
+    <Menu className='w-[300px]'>
       <List
         footer={
           <div>
-            {" "}
-            <Menu.Item key="1" className="text-center">
-              show all{" "}
+            {' '}
+            <Menu.Item key='1' className='text-center'>
+              show all{' '}
             </Menu.Item>
           </div>
         }
@@ -50,18 +59,18 @@ const Header = () => {
     </Menu>
   );
 
-  const name = "show ten nguoi dung";
+  const name = 'show ten nguoi dung';
   const maxLength = 10;
   return (
     <>
-      <Row className="bg-[#171C32] px-5 py-4 text-white items-center	">
+      <Row className='bg-[#171C32] px-5 py-4 text-white items-center	'>
         <Col xs={8} sm={5} md={4} lg={4} xl={3}>
-          <a href="#">
-            <div className="w-4/5">
+          <a href='#'>
+            <div className='w-4/5'>
               <img
                 src={LogoNav}
-                style={{ maxWidth: "100%", height: "auto" }}
-                alt=""
+                style={{ maxWidth: '100%', height: 'auto' }}
+                alt=''
               />
             </div>
           </a>
@@ -69,30 +78,33 @@ const Header = () => {
         <Col xs={0} sm={0} md={7} lg={7} xl={8}>
           <div>
             <Select
-              className="lg:w-1/2 sm:w-full h-[2.5rem] hidden sm:block  "
+              className='lg:w-1/2 sm:w-full h-[2.5rem] hidden sm:block  '
               showSearch
-              defaultValue="1"
-              optionFilterProp="children"
+              defaultValue='1'
+              optionFilterProp='children'
               filterOption={(input, option) =>
-                (option?.label ?? "").toLowerCase().includes(input.toLowerCase())
+                (option?.label ?? '')
+                  .toLowerCase()
+                  .includes(input.toLowerCase())
               }
               filterSort={(optionA, optionB) =>
-                (optionA?.label ?? "")
+                (optionA?.label ?? '')
                   .toLowerCase()
-                  .localeCompare((optionB?.label ?? "").toLowerCase())
+                  .localeCompare((optionB?.label ?? '').toLowerCase())
               }
+              onChange={handleSelectChange}
               options={[
                 {
-                  value: "1",
-                  label: "Da Nang",
+                  value: '1',
+                  label: 'Da Nang',
                 },
                 {
-                  value: "2",
-                  label: "Ha Noi",
+                  value: '2',
+                  label: 'Ha Noi',
                 },
                 {
-                  value: "6",
-                  label: "Hoi Chi Minh",
+                  value: '6',
+                  label: 'Ho Chi Minh',
                 },
               ]}
             />
@@ -104,21 +116,21 @@ const Header = () => {
           md={11}
           lg={12}
           xl={12}
-          className="flex justify-end lg:gap-10 gap-4"
+          className='flex justify-end lg:gap-10 gap-4'
         >
-          <div className="flex gap-4 items-center">
+          <div className='flex gap-4 items-center'>
             <LanguageSwitcher />
 
-            <GiftOutlined style={{ fontSize: "1.4rem" }} />
-            <MessageOutlined style={{ fontSize: "1.4rem" }} />
+            <GiftOutlined style={{ fontSize: '1.4rem' }} />
+            <MessageOutlined style={{ fontSize: '1.4rem' }} />
 
-            <Dropdown overlay={menu} placement="bottom" arrow>
-              <BellOutlined style={{ fontSize: "1.4rem" }} />
+            <Dropdown overlay={menu} placement='bottom' arrow>
+              <BellOutlined style={{ fontSize: '1.4rem' }} />
             </Dropdown>
           </div>
-          <div className="flex gap-3 items-center ">
-            <img src={Ava} alt="avatar" className="w-9" />
-            <p className="truncate hidden sm:block">
+          <div className='flex gap-3 items-center '>
+            <img src={Ava} alt='avatar' className='w-9' />
+            <p className='truncate hidden sm:block'>
               {truncateName(name, maxLength)}
             </p>
           </div>
@@ -126,8 +138,8 @@ const Header = () => {
 
         <Col xs={1} sm={1} md={1} lg={0} xl={0}>
           <div>
-            <MenuOutlined className="text-2xl , ml-6" onClick={toggleSidebar} />
-            <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar}/>
+            <MenuOutlined className='text-2xl , ml-6' onClick={toggleSidebar} />
+            <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
           </div>
         </Col>
       </Row>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,25 @@
+import { CalendarOutlined, UsergroupAddOutlined } from '@ant-design/icons';
+import Search from 'antd/es/input/Search';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import CardComponent from '../components/CardComponent';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 
-import { useEffect, useState } from 'react';
-import CardComponent from '../components/CardComponent';
 const HomePage = () => {
   const { t } = useTranslation();
   const [locations, setLocations] = useState([]);
+  const [inputSearch, setInputSearch] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    fetchLocations();
-  }, []);
+    fetchLocations(searchTerm, inputSearch);
+  }, [searchTerm, inputSearch]);
 
-  const fetchLocations = async (searchTerm = '') => {
+  const fetchLocations = async (param1, param2) => {
     try {
       const response = await fetch(
-        `http://localhost:3001/search/locations?name=${searchTerm}`
+        `http://localhost:3001/search/locations?name=${param1}&workplace?name=${param2}`
       );
       const data = await response.json();
       setLocations(data);
@@ -24,12 +28,52 @@ const HomePage = () => {
     }
   };
 
+  const handleSearchTermChange = (searchTerm) => {
+    setSearchTerm(searchTerm);
+  };
+
   return (
     <div>
-      <Header />
-      <h1>{t('hello')}</h1>
-      <p>{t('welcome')}</p>
-
+      <Header onSearchTermChange={handleSearchTermChange} />
+      <div className=' md:flex justify-between my-4 block'>
+        <div className='md:flex items-center block'>
+          <h1 className='text-[28px] font-medium mb-[14px] lg:mb-0'>
+            {t('Workplaces')}
+          </h1>
+          <Search
+            className='md:pl-[112px] pl-0 w-[234px] md:w-full'
+            placeholder='Search space'
+            value={inputSearch}
+            onChange={(e) => {
+              setInputSearch(e.target.value);
+            }}
+          />
+        </div>
+        <div className='flex gap-4 items-center text-[15px] mt-[15px] md:mt-0 xs:hidden sm:flex'>
+          <a
+            className='border-2 px-[10px] py-[5px] hover:bg-[#d0d0d0]'
+            href='#'
+          >
+            <CalendarOutlined className='pr-[4px]' />
+            Month View
+          </a>
+          <a className='border-2 px-[10px] py-[5px] flex bg-[#d0d0d0]' href='#'>
+            <img
+              className='pr-[4px]'
+              src='https://conext.asia/assets/Mothlybudget-a217f820.svg'
+              alt=''
+            />
+            Lowest Price
+          </a>
+          <a
+            className='border-2 px-[10px] py-[5px] hover:bg-[#d0d0d0]'
+            href='#'
+          >
+            <UsergroupAddOutlined className='pr-[4px]' />
+            Most Meet Up
+          </a>
+        </div>
+      </div>
       {locations &&
         locations[0]?.Workplaces?.map((item) => (
           <CardComponent key={item.id} data={item} />
